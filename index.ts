15627bdc99af3c864d9bd107e91e288538fb7432
SenorGrande <clooneyjh@gmail.com>
2021-12-10 19:39:53 +1300

    fix: change back to task variable stage variable not passed to bash task

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as aws from "@pulumi/aws";
 
-const env = process.env.environment_name;
+const env = process.env.ENVIRONMENT;
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket(`my-bucket-${env}`);
